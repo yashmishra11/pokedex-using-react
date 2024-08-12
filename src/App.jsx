b13dbc09173a94f5f2ff40df75683b94bd624688
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2024-08-12 14:31:22 +0530

    Updated App.jsx for the song to play

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import CustomRoutes from "./routes/CustomRoutes";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./App.css";
+import React, { useEffect, useRef } from 'react';
 
 const App = () => {
+  const audioRef = useRef(null);
+
+  const handlePlayAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play().catch(error => {
+        console.error("Audio failed to play:", error);
+      });
+    }
+  };
+
   return (
-    <div className="outer-pokedex">
-      <h1 id = "pokedex-heading">
-        <Link className="pokedex-heading" to ="/">Pokedex
-        </Link>
+    <div className="outer-pokedex" onClick={handlePlayAudio}>
+      <h1 id="pokedex-heading">
+        <audio ref={audioRef} src="/pokemon-instu.wav" />
+        <Link className="pokedex-heading" to="/">Pokedex</Link>
       </h1>
-      <CustomRoutes/>
+      <CustomRoutes />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
